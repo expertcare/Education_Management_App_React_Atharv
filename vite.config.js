a2d9ec2bf1c46c3b03c6d0348070fb72ddfb2ae8
expertcare <expertcarewt@gmail.com>
2024-06-14 21:52:02 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      // "/api": "https://education-management-server-ruby.vercel.app",
+      "/api": "https://education-management-server-ruby.vercel.app",
 
-      "/api": " https://education-management-server.onrender.com",
+      // "/api": " https://education-management-server.onrender.com",
 
       // "/api": "http://localhost:3000",
     },
